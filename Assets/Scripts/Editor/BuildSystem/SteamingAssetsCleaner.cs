4cfb94a13363f1f691961521142c56b1013ed4b2
Lepi009 <60300421+Lepi009@users.noreply.github.com>
2025-12-06 11:47:44 +0100

    [Fix] Addressable Assets

--- Assets/Scripts/Editor/BuildSystem/SteamingAssetsCleaner.cs
+++ Assets/Scripts/Editor/BuildSystem/SteamingAssetsCleaner.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Newtonsoft.Json;
+using Unity.Plastic.Newtonsoft.Json;
 using UnityEngine;
 
 namespace UnityBuilder {
